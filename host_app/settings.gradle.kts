53a4c7f2fbf7b8cdd8822754b4492f270c4014ab
Gray Mackall <mackall@google.com>
2024-10-07 14:10:24 -0700

    First commit

---
+++ host_app/settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        val flutterStorageUrl = System.getenv("FLUTTER_STORAGE_BASE_URL") ?: "https://storage.googleapis.com"
+        maven("$flutterStorageUrl/download.flutter.io")
+    }
+}
+
+rootProject.name = "My Application"
+include(":app")
+// setBinding(new Binding([gradle: this]))                                // new
+println(rootProject.projectDir)
+apply(from = File(settingsDir.parentFile.toString() + "/blahblah/.android/include_flutter.groovy"))
